5a0ac3ac2daec73dfe47a933092cb9b2d61fe94b
Dan Kovacek <dankovacek@gmail.com>
2025-08-12 21:28:10 -0700

    Update documentation

--- _static/searchindex.js
+++ _static/searchindex.js
@@ -41,7 +41,7 @@ function filterStations() {
   limitedMatches.forEach(c => {
     const div = document.createElement('div');
     div.className = 'search-result';
-    div.innerHTML = `<a href='${c.folder}'>$${c.id}: ${c.name}</a>`;
+    div.innerHTML = `<a href='${c.folder}'>${c.id}: ${c.name}</a>`;
     results.appendChild(div);
   });
 
